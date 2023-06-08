5b6f4f41e07ec77301a6eecad7853f17d6a06575
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2023-06-08 16:49:09 -0300

    config comments and edgy

---
+++ lua/plugins/windows-edgy.lua
@@ -0,0 +1,209 @@
+return {
+  {
+    "simrat39/symbols-outline.nvim",
+    opts = {
+      highlight_hovered_item = true,
+      show_guides = true,
+      auto_preview = true,
+      position = "right",
+      relative_width = true,
+      width = 25,
+      auto_close = false,
+      show_numbers = true,
+      show_relative_numbers = false,
+      show_symbol_details = true,
+      preview_bg_highlight = "Pmenu",
+      autofold_depth = nil,
+      auto_unfold_hover = true,
+      fold_markers = { "", "" },
+      wrap = true,
+      keymaps = { -- These keymaps can be a string or a table for multiple keys
+        close = { "<Esc>", "q" },
+        goto_location = "<Cr>",
+        focus_location = "o",
+        hover_symbol = "<C-space>",
+        toggle_preview = "K",
+        rename_symbol = "r",
+        code_actions = "a",
+        fold = "h",
+        unfold = "l",
+        fold_all = "W",
+        unfold_all = "E",
+        fold_reset = "R",
+      },
+      lsp_blacklist = {},
+      symbol_blacklist = {},
+      symbols = {
+        File = { icon = "", hl = "@text.uri" },
+        Module = { icon = "", hl = "@namespace" },
+        Namespace = { icon = "", hl = "@namespace" },
+        Package = { icon = "", hl = "@namespace" },
+        Class = { icon = "𝓒", hl = "@type" },
+        Method = { icon = "ƒ", hl = "@method" },
+        Property = { icon = "", hl = "@method" },
+        Field = { icon = "", hl = "@field" },
+        Constructor = { icon = "", hl = "@constructor" },
+        Enum = { icon = "ℰ", hl = "@type" },
+        Interface = { icon = "ﰮ", hl = "@type" },
+        Function = { icon = "", hl = "@function" },
+        Variable = { icon = "", hl = "@constant" },
+        Constant = { icon = "", hl = "@constant" },
+        String = { icon = "𝓐", hl = "@string" },
+        Number = { icon = "#", hl = "@number" },
+        Boolean = { icon = "⊨", hl = "@boolean" },
+        Array = { icon = "", hl = "@constant" },
+        Object = { icon = "⦿", hl = "@type" },
+        Key = { icon = "🔐", hl = "@type" },
+        Null = { icon = "NULL", hl = "@type" },
+        EnumMember = { icon = "", hl = "@field" },
+        Struct = { icon = "𝓢", hl = "@type" },
+        Event = { icon = "🗲", hl = "@type" },
+        Operator = { icon = "+", hl = "@operator" },
+        TypeParameter = { icon = "𝙏", hl = "@parameter" },
+        Component = { icon = "", hl = "@function" },
+        Fragment = { icon = "", hl = "@constant" },
+      },
+    },
+  },
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
+    keys = {
+      -- close window
+      ["q"] = function(win)
+        win:close()
+      end,
+      -- hide window
+      ["<c-q>"] = function(win)
+        win:hide()
+      end,
+      -- close sidebar
+      ["Q"] = function(win)
+        win.view.edgebar:close()
+      end,
+      -- next open window
+      ["]w"] = function(win)
+        win:next({ visible = true, focus = true })
+      end,
+      -- previous open window
+      ["[w"] = function(win)
+        win:prev({ visible = true, focus = true })
+      end,
+      -- next loaded window
+      ["]W"] = function(win)
+        win:next({ pinned = false, focus = true })
+      end,
+      -- prev loaded window
+      ["[W"] = function(win)
+        win:prev({ pinned = false, focus = true })
+      end,
+    },
+    ---@type table<Edgy.Pos, {size:integer, wo?:vim.wo}>
+    options = {
+      left = { size = 30 },
+      bottom = { size = 10 },
+      right = { size = 30 },
+      top = { size = 10 },
+    },
+    -- edgebar animations
+    animate = {
+      enabled = true,
+      fps = 100, -- frames per second
+      cps = 120, -- cells per second
+      on_begin = function()
+        vim.g.minianimate_disable = true
+      end,
+      on_end = function()
+        vim.g.minianimate_disable = false
+      end,
+      -- Spinner for pinned views that are loading.
+      -- if you have noice.nvim installed, you can use any spinner from it, like:
+      -- spinner = require("noice.util.spinners").spinners.circleFull,
+      spinner = {
+        frames = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+        interval = 80,
+      },
+    },
+    -- enable this to exit Neovim when only edgy windows are left
+    exit_when_last = false,
+    -- global window options for edgebar windows
+    ---@type vim.wo
+    wo = {
+      -- Setting to `true`, will add an edgy winbar.
+      -- Setting to `false`, won't set any winbar.
+      -- Setting to a string, will set the winbar to that string.
+      winbar = true,
+      winfixwidth = true,
+      winfixheight = true,
+      winhighlight = "WinBar:EdgyWinBar,Normal:EdgyNormal",
+      spell = true,
+      signcolumn = "no",
+    },
+    icons = {
+      closed = " ",
+      open = " ",
+    },
+    opts = {
+      bottom = {
+        { ft = "toggleterm", size = { height = 0.4 } },
+        {
+          ft = "lazyterm",
+          title = "LazyTerm",
+          size = { height = 0.4 },
+          filter = function(buf)
+            return not vim.b[buf].lazyterm_cmd
+          end,
+        },
+        "Trouble",
+        { ft = "qf", title = "QuickFix" },
+        {
+          ft = "help",
+          size = { height = 20 },
+          -- don't open help files in edgy that we're editing
+          filter = function(buf)
+            return vim.bo[buf].buftype == "help"
+          end,
+        },
+        { ft = "spectre_panel", size = { height = 0.4 } },
+      },
+      left = {
+        {
+          title = "Neo-Tree",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          size = { height = 0.5 },
+        },
+        {
+          title = "Neo-Tree Git",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "git_status"
+          end,
+          pinned = true,
+          open = "Neotree position=right git_status",
+        },
+        {
+          title = "Neo-Tree Buffers",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "buffers"
+          end,
+          pinned = true,
+          open = "Neotree position=top buffers",
+        },
+        {
+          ft = "Outline",
+          pinned = true,
+          open = "SymbolsOutline",
+        },
+        "neo-tree",
+      },
+    },
+  },
+}
